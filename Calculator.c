c74fefb80ce559ba24447c4da09aa475851b56f4
Aryan Shahi <rnshahi34@gmail.com>
2025-01-02 10:45:39 +0545

    Update in Code

--- Calculator.c
+++ Calculator.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-void sum();
-void product();
-void subtract();
-void divide();
-
-float num1, num2, result;
+float sum(float a, float b);
+float product(float a, float b);
+float subtract(float a, float b);
+float divide(float a, float b);
 
 void main()
 {
-
+    float num1, num2, result;
     int choice;
     char qw;
     printf("---------------------------------------------------------\n");
@@ -29,46 +27,48 @@ void main()
         switch (choice)
         {
         case 1:
-            sum();
+            result = sum(num1, num2);
             break;
         case 2:
-            subtract();
+            result = subtract(num1, num2);
             break;
         case 3:
-            product();
+            result = product(num1, num2);
             break;
         case 4:
-            divide();
+            result = divide(num1, num2);
             break;
         default:
             printf("Error in choice!\n");
         }
+        if (choice > 0 && choice < 5)
+        {
+            printf("The result of operation is %f\n", result);
+        }
+
         printf("---------------------------------------------------------\n");
         printf("Do you want to continute?(Y/N):");
         scanf(" %c", &qw);
     } while (qw == 'y' || qw == 'Y');
+    printf("Closing Calculator...\n");
 }
 
-void sum()
+float sum(float a, float b)
 {
-    result = num1 + num2;
-    printf("The result of sum is:%f\n", result);
+    return (a + b);
 }
 
-void subtract()
+float subtract(float a, float b)
 {
-    result = num1 - num2;
-    printf("The result of subtraction is:%f\n", result);
+    return (a - b);
 }
 
-void product()
+float product(float a, float b)
 {
-    result = num1 * num2;
-    printf("The result of multiplication is:%f\n", result);
+    return (a * b);
 }
 
-void divide()
+float divide(float a, float b)
 {
-    result = num1 / num2;
-    printf("The result of division is:%f\n", result);
+    return (a / b);
 }
